2e70e43957f54b0e144858b8fda287b40122d23d
hasanthikaaa <hasanthika@axis-tech.co>
2024-06-24 10:22:56 +0530

    initial commit

---
+++ test/aws-cdk-dynamodb-cognito-cicd.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkDynamodbCognitoCicd from '../lib/aws-cdk-dynamodb-cognito-cicd-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-dynamodb-cognito-cicd-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkDynamodbCognitoCicd.AwsCdkDynamodbCognitoCicdStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
